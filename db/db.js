f16c94b6af8e4279badabcd6f10fbb07138aa10e
RaoUmair55 <raoumair554@gmail.com>
2025-06-04 14:48:09 +0500

    refactor: update MongoDB connection string to use placeholder values

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://hackthecode404:C32PwILkUVb9hHF1@cluster0.jbfubzz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/urlShortener');
+        await mongoose.connect('mongodb+srv://username:password@cluster0.jbfubzz.mongodb.net/?retryWrites=true&w=majority');
         console.log("MongoDB connected");
     } catch (error) {
         console.error("MongoDB connection error:", error);
